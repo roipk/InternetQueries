d839993ea24c05c546cb7a7eaffa7c9444695c68
Gili Lis <gilis100@gmail.com>
2019-12-04 14:06:18 +0200

    find stars

--- Ex1/Ex1.py
+++ Ex1/Ex1.py
@@ -1,4 +1,21 @@
-print("test")
-print ("only test")
-print("test again")
-print("Hello")
+import sys
+import struct
+import numpy as np
+
+with open("100.txt","rb") as text_file:
+    # One option is to call readline() explicitly
+    # single_line = text_file.readline()
+
+    # It is easier to use a for loop to iterate each line
+    doc_list = np.array([])
+    stars='{}\r\n'.format(('*'* 80))
+    counter=0
+    for line in text_file:
+        if(line != stars):
+            if(line !='\r\n'):
+                counter=counter+1
+                np.insert(doc_list,1,counter)
+                print(counter)
+            # print (line)
+   # data = text_file.read()
+   # print(data)
